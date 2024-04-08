964bf4f2f93d79e46620018ace6b39560b73e4a8
SubhaDeviNunna <135640664+SubhaDeviNunna@users.noreply.github.com>
2024-04-08 14:24:02 +0530

    Update OddPositionCharacters.java

--- Assignment2/OddPositionCharacters.java
+++ Assignment2/OddPositionCharacters.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class OddPositionCharacters {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String text = "type here to search";
 
-        System.out.print("Enter a string: ");
-        String text = scanner.nextLine();
-
-        printOddPositionCharacters(text);
-
-        scanner.close();
+        String oddPositionChars = getOddPositionCharacters(text);
+        System.out.println("Odd position characters (ignoring whitespace): " + oddPositionChars);
     }
 
-    public static void printOddPositionCharacters(String text) {
-        text = text.replaceAll("\\s+", ""); // Remove white space
+    public static String getOddPositionCharacters(String text) {
+        StringBuilder oddPositionChars = new StringBuilder();
+
         for (int i = 0; i < text.length(); i++) {
-            if (i % 2 == 0) {
-                System.out.print(text.charAt(i));
+            if (text.charAt(i) != ' ' && i % 2 != 0) {
+                oddPositionChars.append(text.charAt(i));
             }
         }
+
+        return oddPositionChars.toString();
     }
 }
